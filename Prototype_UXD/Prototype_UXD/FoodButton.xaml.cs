c48cedcdfaf8c2fcc03b65629a0c07525f33c581
Cellmon95 <lucas.arne@hotmail.com>
2014-12-15 13:33:18 +0100

    Everything

---
+++ Prototype_UXD/Prototype_UXD/FoodButton.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prototype_UXD
+{
+	/// <summary>
+	/// Interaction logic for FoodButton.xaml
+	/// </summary>
+	public partial class FoodButton : UserControl
+	{
+		private MainWindow mainWindow;
+
+		public FoodButton()
+		{
+			this.InitializeComponent(); 
+		}
+		private void Confirm_Loaded(object sender, RoutedEventArgs e)
+		{
+			mainWindow = Application.Current.MainWindow as MainWindow;
+		}
+
+
+		public Brush Picture
+		{
+			get
+			{
+				return FoodPicture.Fill;
+			}
+			set
+			{
+				FoodPicture.Fill = value;
+			}
+		}
+
+		public String FoodName { get; set; }
+
+		private void Select_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			mainWindow.changeLayout(MainWindow.Layouts.secondPage);
+			mainWindow.FoodPicture.Fill = FoodPicture.Fill;
+			mainWindow.FoodName_txb.Text = FoodName;
+		}
+	}
+}
---
+++ Prototype_UXD/Prototype_UXD/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prototype_UXD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		public enum Layouts
+		{
+			firstPage,
+			secondPage
+		}
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+			firstPage.Visibility = Visibility.Visible;
+			secondPage.Visibility = Visibility.Collapsed;
+			thirdPage.Visibility = Visibility.Collapsed;
+			fourthPage.Visibility = Visibility.Collapsed;
+
+			KottbullarOPotatismos.FoodName = "Köttbullar och potatis";
+			LaxOPotatis.FoodName = "Lax och potatis";
+			Sallad.FoodName = "Sallad";
+			GlutenfrittBrod.FoodName = "Glutenfritt Bröd";
+			KycklingOCurry.FoodName = "Kyckling Och Curry";
+			GottOBlandat.FoodName = "Gott Och Blandat";
+
+			Description_txtb.IsReadOnly = true;
+        }
+
+		public void changeLayout(Layouts layout)
+		{
+			switch (layout)
+			{
+				case Layouts.firstPage:
+
+					break;
+				case Layouts.secondPage:
+					firstPage.Visibility = Visibility.Collapsed;
+					secondPage.Visibility = Visibility.Visible;
+					break;
+				default:
+					break;
+			}
+		}
+
+		private void Select_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			secondPage.Visibility = Visibility.Collapsed;
+			thirdPage.Visibility = Visibility.Visible;
+		}
+
+		private void Back_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			secondPage.Visibility = Visibility.Collapsed;
+			firstPage.Visibility = Visibility.Visible;
+		}
+
+		private void Select_rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			thirdPage.Visibility = Visibility.Collapsed;
+			fourthPage.Visibility = Visibility.Visible;
+		}
+
+		private void Back_rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			thirdPage.Visibility = Visibility.Collapsed;
+			secondPage.Visibility = Visibility.Visible;
+		}
+
+		//public Brush Picture
+		//{
+		//	get
+		//	{
+		//		return FoodPicture.Fill;
+		//	}
+		//	set
+		//	{
+		//		FoodPicture.Fill = value;
+		//	}
+		//}
+	}
+}
